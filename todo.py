78ca78f881af96ca93b72cc54d4c6c686895863f
Sneha-Va <sneha.varghese@nestgroup.net>
2022-11-03 20:05:44 +0530

    first commit

---
+++ todo.py
@@ -0,0 +1,19 @@
+import requests
+import json
+import mysql.connector
+import sys
+try:
+    mydb=mysql.connector.connect(host='localhost',user='root',password='',database='tododb')
+except mysql.connector.Error as e:
+    sys.exit("connection error")
+mycursor=mydb.cursor()
+data=requests.get("https://jsonplaceholder.typicode.com/todos").text
+data_info=json.loads(data)
+for i in data_info:
+    if(i['completed']==False):
+        id=str(i['userId'])
+        complete=str(i['completed'])
+        sql="INSERT INTO `todos`(`userid`, `title`, `completed`) VALUES ('"+id+"','"+i['title']+"','"+complete+"')"
+        mycursor.execute(sql)
+        mydb.commit()
+        print("data successfully inserted")
